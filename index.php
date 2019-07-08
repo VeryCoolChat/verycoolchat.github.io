8625145c30120f5d64ae7d57af1d03f3e4fa6f08
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-08 13:33:55 -0400

    Gitacora saved 88 changed files

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ function myFunction(){
   </tr>
   
 	<tr>
-    
+
     <td style="font-family: sans-serif;font-size: 17pt;text-align: left; width: 432px; color: white;">
       <center>Select:</center>
     </td>
@@ -106,6 +106,7 @@ function myFunction(){
         </select>
       </center>
     </td>
+    
   </tr>
   
 	<tr>
