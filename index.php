099a2066f6b2145492df05672b043250875b9c45
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-08 14:09:19 -0400

    Gitacora saved 2 changed files

--- index.php
+++ index.php
@@ -39,22 +39,15 @@ if (isset($_GET['enSubmit']) && isset($_GET['uname']) && isset($_GET['rname'])){
 <!DOCTYPE html>
 <html>
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset= utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset= utf-8">
 
-  <script>
-  function myFunction(){
-    alert("testing123");
-  };
-  </script>
+<script>
+function myFunction(){
+	alert("testing123");
+};
+</script>
 
-  <!-- The core Firebase JS SDK is always required and must be listed first -->
-  <script src="/__/firebase/6.2.4/firebase-app.js"></script>
 
-  <!-- TODO: Add SDKs for Firebase products that you want to use
-      https://firebase.google.com/docs/web/setup#reserved-urls -->
-
-  <!-- Initialize Firebase -->
-  <script src="/__/firebase/init.js"></script>
 
 </head>
 
