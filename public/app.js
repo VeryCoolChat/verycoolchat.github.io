ea0b5e6cc471f7c0324b404745ffbe5e9d97592b
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-09 13:24:19 -0400

    Gitacora saved 2 changed files

--- public/app.js
+++ public/app.js
@@ -6,6 +6,6 @@ document.addEventListener("DOMContentLoaded", event => {
 
     chatHistory.onSnapshot(doc => {
         const chatData = doc.data();
-        document.write(data.title + )
+        document.write(data.title + `br`)
     })
 });
